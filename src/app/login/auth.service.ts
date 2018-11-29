6ac063ac0198ea8e1dd551fff931bee585447a78
AlessandroMx <alessandromx@outlook.com>
2018-11-29 07:59:46 -0600

    Access to api in progress...

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -7,9 +7,12 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   getUserDetails(id, password) {
-    return this.http.post('http://localhost:8086/', {
-      id,
-      password
+    return this.http.post('/api', {
+      'operation': 'auth_user',
+      'operation_args': {
+        'id': id,
+        'password': password
+      }
     }).subscribe(data => {
       console.log(data)
     })
